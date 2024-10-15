d6818d5e3762cf952712f8196c895a7155245316
esperancaa <rodrigoce9@gmail.com>
2024-10-15 11:26:21 +0100

    exe 4

---
+++ Exercises/Cosien_distance_missing_tests.py
@@ -0,0 +1,32 @@
+import numpy as np
+
+
+def cosine_distance(x:np.ndarray, y:np.ndarray) -> float:
+    
+    """
+    Calculates the Cosine distance between X and Y using the following formula: similarity(x,y)= x⋅y/∣∣x∣∣∣∣y∣∣  
+    where x⋅y is the dot product of the vectors x and y and ∣∣x∣∣∣∣y∣∣ is the product of the Euclidean norms of x and y.
+ 
+    Parameters
+    ----------
+    x: np.array
+        A numpy array
+    y: np.array
+        A numpy array
+        
+    Returns
+    ----------
+    cosine_distance: float
+        The cosine distance between X and Y
+    
+    """
+    distances= []
+    dot_product = np.dot(x, y)
+    magnitude_x = np.linalg.norm(x)
+    magnitude_y = np.linalg.norm(y)
+    cosine_similarity = dot_product / (magnitude_x * magnitude_y)
+    cosine_distance = 1 - cosine_similarity
+    distances.append(cosine_distance)
+    
+    return distances
+    
